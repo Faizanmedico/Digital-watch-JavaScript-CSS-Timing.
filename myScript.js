7df6dda74e883e22d04038e4c50e1f4d74acdf3e
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-01-24 23:12:24 +0500

    Add files via upload
    Digital watch JavaScript & CSS Timing. These time intervals are called timing events.

---
+++ myScript.js
@@ -0,0 +1,9 @@
+let myVar = setInterval(myTimer ,1000);
+    function myTimer() {
+      const d = new Date();
+      document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+    }
+
+// time 2
+
+    
